a8032c664d2e6a5ba640f2a624f2214d20ddb478
yupei_lin <yupei_lin@asus.com>
2020-11-06 17:11:18 +0800

    Ethernet : enable "Wake-On-Lan" function.
    Change-Id: Ibd0c0883058c6c9c2f1603d92d73897945389d94

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -25,6 +25,7 @@
 #include <linux/of_net.h>
 #include <linux/of_device.h>
 #include <linux/of_mdio.h>
+#include <linux/of_gpio.h>
 
 #include "stmmac.h"
 #include "stmmac_platform.h"
@@ -388,6 +389,8 @@ stmmac_probe_config_dt(struct platform_device *pdev, const char **mac)
 	struct device_node *np = pdev->dev.of_node;
 	struct plat_stmmacenet_data *plat;
 	struct stmmac_dma_cfg *dma_cfg;
+	enum of_gpio_flags flags;
+	const char *wakeup_enable;
 	int rc;
 
 	plat = devm_kzalloc(&pdev->dev, sizeof(*plat), GFP_KERNEL);
@@ -396,6 +399,19 @@ stmmac_probe_config_dt(struct platform_device *pdev, const char **mac)
 
 	*mac = of_get_mac_address(np);
 	plat->interface = of_get_phy_mode(np);
+	plat->wolirq_io = of_get_named_gpio_flags(np, "wolirq-gpio", 0, &flags);
+
+	/* Get wakeup_enable */
+	if (of_property_read_string(np, "wakeup-enable", &wakeup_enable)) {
+		printk("[WOL] Fail to read wakeup-enable");
+		plat->wakeup_enable = 0;
+	} else {
+		printk("[WOL] wakeup_enable = %s", wakeup_enable);
+		if (!strcmp(wakeup_enable, "1"))
+			plat->wakeup_enable = 1;
+		else
+			plat->wakeup_enable = 0;
+	}
 
 	/* Get max speed of operation from device tree */
 	if (of_property_read_u32(np, "max-speed", &plat->max_speed))
